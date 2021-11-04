ee403490369a22fab85fe5f0d55930424edb0043
CodinGitHub <gms2programador@gmail.com>
2021-11-04 05:49:51 -0500

    first commit

---
+++ users.php
@@ -0,0 +1,21 @@
+<?php 
+  function getUsers(){
+
+  }
+
+  function getUsersById(){
+
+  }
+
+  function createUser(){
+
+  }
+
+  function updateUser($data, $id){
+
+  }
+
+  function deleteUser($id){
+    
+  }
+?>
